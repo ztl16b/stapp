0a3a80c9c16e6817a581e16eda404c1aa6608d6f
Zack Lung <zacklung@Zacks-MBP.localdomain>
2025-04-10 18:49:42 -0600

    17

--- celery_worker.py
+++ celery_worker.py
@@ -30,6 +30,13 @@ app.conf.update(
     result_serializer='json',
     timezone='UTC',
     enable_utc=True,
+    broker_connection_retry_on_startup=True,
+    broker_connection_max_retries=10,
+    broker_connection_timeout=30,
+    task_time_limit=300,
+    task_soft_time_limit=240,
+    worker_prefetch_multiplier=1,
+    worker_max_tasks_per_child=100,
 )
 
 app.autodiscover_tasks(['tasks'])
